d91182e57b027be693f2756c758cb5c9baad4c71
luciledubarry <luciledubarry@gmail.com>
2025-07-23 10:32:30 +0200

    Nouvelle version du master file, en ajoutant les bases Cultivation oper/plot/hh

--- ICRESAT_database_analysis/Master_file.R
+++ ICRESAT_database_analysis/Master_file.R
@@ -7,10 +7,12 @@ rm(list = ls())
 graphics.off()
 cat("\014")
 
-here::i_am("ICRESAT_database_analysis/Master_file.R")
+here::i_am("ICRESAT_database_analysis/Master_file_tables.R")
+
 
 # Sélectionner les années
 annees <- 2011:2014
+
 
 # Lancer les fichiers CultData1 et CultData2 pour chaque année
 for (annee in annees) {
@@ -20,7 +22,7 @@ for (annee in annees) {
     input = here::here("ICRESAT_database_analysis", "CultData1_merging_V2.qmd"),
     execute_params = list(annee = annee),
     output_file = paste0("CultData1_merging_", annee, ".html")
-    )
+  )
   
   quarto::quarto_render(
     input = here::here("ICRESAT_database_analysis", "CultData2_cleaning_V2.qmd"),
@@ -29,11 +31,51 @@ for (annee in annees) {
   )
 }
 
-list_bases <- list()
 
-# Enregistrer les tables par année
+# Fonction pour joindre les années et enregistrer
+merge_save_files <- function(name_file) {
+  list_bases <- list()
+  
+  for (annee in annees) {
+    fichier_rds <- here("Base de données générées", name_file, paste0(name_file, "_", annee, ".rds"))
+    
+    if (file.exists(fichier_rds)) {
+      base <- readRDS(fichier_rds)
+      base$YEAR <- annee
+      list_bases[[as.character(annee)]] <- base
+    } else {
+      message("Fichier manquant pour l'année ", annee, " pour ", name_file)
+    }
+  }
+  
+  df_all <- bind_rows(list_bases) |>
+    relocate(YEAR, .before = VDS_ID)
+  
+  folder_path <- here("Base de données générées", name_file)
+  
+  write.csv(df_all, file = file.path(folder_path, paste0(name_file, "_all.csv")), row.names = FALSE)
+  saveRDS(df_all, file = file.path(folder_path, paste0(name_file, "_all.rds")))
+  write_dta(df_all, path = file.path(folder_path, paste0(name_file, "_all.dta")))
+  
+  return(df_all)
+}
+
+
+# Appliquer à chaque table
+Cultivation_expand_all <- merge_save_files("Cultivation_expand")
+Cultivation_oper_all <- merge_save_files("Cultivation_oper")
+Cultivation_plot_all <- merge_save_files("Cultivation_plot")
+Cultivation_hh_all   <- merge_save_files("Cultivation_hh")
+
+
+
+
+################################################################################
+
+# Au niveau de l'opération : Cultivation_oper
+
 for (annee in annees) {
-  fichier_rds <- here::here("Base de données générées", "Cultivation_expand", paste0("Cultivation_expand_", annee, ".rds"))
+  fichier_rds <- here::here("Base de données générées", "Cutlivation_oper", paste0("Cultivation_oper_", annee, ".rds"))
   if (file.exists(fichier_rds)) {
     base <- readRDS(fichier_rds)
     base$YEAR <- annee  # Ajouter la variable année
@@ -43,25 +85,86 @@ for (annee in annees) {
   }
 }
 
-# Fusionner les années
-Cultivation_expand_all <- bind_rows(list_bases)
-Cultivation_expand_all <- Cultivation_expand_all |> relocate(YEAR, .before = VDS_ID)
+Cultivation_oper_all <- bind_rows(list_bases)
+Cultivation_oper_all <- Cultivation_oper_all |> relocate(YEAR, .before = VDS_ID)
 
-# Enregistrer la table finale
-folder_path <- here("Base de données générées", "Cultivation_expand")
-
+folder_path <- here("Base de données générées", "Cultivation_oper")
 write.csv(
-  Cultivation_expand_all,
-  file = file.path(folder_path, "Cultivation_expand_all.csv"),
+  Cultivation_oper_all,
+  file = file.path(folder_path, "Cultivation_oper_all.csv"),
   row.names = FALSE
 )
-
 saveRDS(
-  Cultivation_expand_all, 
-  file = file.path(folder_path, "Cultivation_expand_all.rds")
+  Cultivation_oper_all, 
+  file = file.path(folder_path, "Cultivation_oper_all.rds")
+)
+write_dta(
+  Cultivation_oper_all,
+  path = file.path(folder_path, "Cultivation_oper_all.dta")
 )
 
-write_dta(
-  Cultivation_expand_all,
-  path = file.path(folder_path, "Cultivation_expand_all.dta")
+
+# Au niveau du champ : Cultivation_plot
+
+for (annee in annees) {
+  fichier_rds <- here::here("Base de données générées", "Cultivation_plot", paste0("Cultivation_plot_", annee, ".rds"))
+  if (file.exists(fichier_rds)) {
+    base <- readRDS(fichier_rds)
+    base$YEAR <- annee  # Ajouter la variable année
+    list_bases[[as.character(annee)]] <- base
+  } else {
+    message("Fichier manquant pour l'année ", annee)
+  }
+}
+
+Cultivation_plot_all <- bind_rows(list_bases)
+Cultivation_plot_all <- Cultivation_plot_all |> relocate(YEAR, .before = VDS_ID)
+
+folder_path <- here("Base de données générées", "Cultivation_plot")
+write.csv(
+  Cultivation_plot_all,
+  file = file.path(folder_path, "Cultivation_plot_all.csv"),
+  row.names = FALSE
 )
+saveRDS(
+  Cultivation_plot_all, 
+  file = file.path(folder_path, "Cultivation_plot_all.rds")
+)
+write_dta(
+  Cultivation_plot_all,
+  path = file.path(folder_path, "Cultivation_plot_all.dta")
+)
+
+
+# Au niveau du ménage : Cultivation_hh
+
+for (annee in annees) {
+  fichier_rds <- here::here("Base de données générées", "Cultivation_hh", paste0("Cultivation_hh_", annee, ".rds"))
+  if (file.exists(fichier_rds)) {
+    base <- readRDS(fichier_rds)
+    base$YEAR <- annee  # Ajouter la variable année
+    list_bases[[as.character(annee)]] <- base
+  } else {
+    message("Fichier manquant pour l'année ", annee)
+  }
+}
+
+Cultivation_hh_all <- bind_rows(list_bases)
+Cultivation_hh_all <- Cultivation_hh_all |> relocate(YEAR, .before = VDS_ID)
+
+folder_path <- here("Base de données générées", "Cultivation_hh")
+write.csv(
+  Cultivation_hh_all,
+  file = file.path(folder_path, "Cultivation_hh_all.csv"),
+  row.names = FALSE
+)
+saveRDS(
+  Cultivation_hh_all, 
+  file = file.path(folder_path, "Cultivation_hh_all.rds")
+)
+write_dta(
+  Cultivation_hh_all,
+  path = file.path(folder_path, "Cultivation_hh_all.dta")
+)
+
+
