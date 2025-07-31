0ce7b8a7417fc5a0867f2f8be401cb72693dca8b
luciledubarry <luciledubarry@gmail.com>
2025-07-31 15:53:03 +0200

    Créer Master_file_analysis + renommer les fichers Analysis

---
+++ ICRESAT_database_analysis/scripts_all_years/Master_file_analysis.R
@@ -0,0 +1,28 @@
+library(here)
+library(quarto)
+library(dplyr)
+library(haven)
+library(fs)
+
+rm(list = ls())
+graphics.off()
+cat("\014")
+
+here::i_am("ICRESAT_database_analysis/scripts_all_years/Master_file_analysis.R")
+
+
+# Sous-dossier pour les sorties html
+output_dir <- here("ICRESAT_database_analysis/scripts_all_years", "outputs_html")
+
+
+# Lancer les fichiers d'analyse
+files_to_render <- c("Regression_labor", "Analysis_production", "Analysis_mecanisation", "Analysis_gender", "Analysis_caste")
+
+for (file in files_to_render) {
+  cat("Traitement du fichier :", file, "\n")
+  quarto::quarto_render(
+    input = here("ICRESAT_database_analysis/scripts_all_years", paste0(file, ".qmd")),
+    output_file = paste0(file, ".html")
+  )
+  file_move(paste0(file, ".html"), path(output_dir, paste0(file, ".html")))
+}
